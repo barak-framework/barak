d964a434e9fa4b5f89c50a24bc46a51112415fe3
Gökhan Demir <mail@gdemir.me>
2017-07-12 15:26:48 +0300

    Update ApplicationCache.php

--- lib/ApplicationCache.php
+++ lib/ApplicationCache.php
@@ -1,11 +1,17 @@
 <?php
 
 class ApplicationCache {
-  public static function set() {}
-  public static function get() {}
-  public static function del() {}
-  public static function has() {}
+  
+  public static function set($key, $value) {}
+
+  public static function get($key) {}
+
+  public static function del($key) {}
+
+  public static function has($key) {}
+
   public static function reset() {}
+
   private static function storage_key() {
     return '_cache';
   }
