295bcc5dd8d39875e3cff382cda791f3be81d049
Gökhan Demir <mail@gdemir.me>
2017-07-12 23:29:17 +0300

    Update ApplicationCache.php

--- lib/ApplicationCache.php
+++ lib/ApplicationCache.php
@@ -1,6 +1,5 @@
 <?php
-// #TODO must be test!!!
-// $GLOBALS["cacheexpire"] check in get
+// #TODO storage_key with $GLOBALS["cacheexpire"] check in get
 class ApplicationCache {
 
   const CACHEDIR = "cache/";
@@ -8,19 +7,12 @@ class ApplicationCache {
   public static function set($key, $value, $expiration = 604800) {
 
     // struct of key
-    $data = [
-      'time'   => time(),
-      'expire' => $expiration,
-      'value'  => serialize($value)
-    ];
+    $data = [ 'time' => time(), 'expire' => $expiration, 'value' => serialize($value) ];
 
     $filename = self::filename_format($key);
 
-    // save
-   
-    file_put_contents($filename, json_encode($data));
-    // or 
-    if (!($fh = fopen($filename, 'a')))
+    // file_put_contents($filename, json_encode($data));
+    if (!($fh = fopen($filename, 'w')))
       throw new FileNotFoundException("Cache bellek açılamadı", $filename);
 
     fwrite($fh, json_encode($data));
@@ -35,7 +27,7 @@ class ApplicationCache {
     if (file_exists($filename)) {
 
       // check expire time ? get or del
-      $data = json_decode(include $filename);
+      $data = json_decode(file_get_contents($filename), true);
       if ($data["expire"] > (time() - $data["time"]))
         return unserialize($data["value"]);
       else
@@ -45,27 +37,25 @@ class ApplicationCache {
   }
 
   public static function del($key) {
-    
-    // get filename
     $filename = self::filename_format($key);
     if (file_exists($filename))
       unlink($filename);
   }
 
   public static function has($key) {
-    return (file_exists(self::filename_format($key))) ? true : false; 
+    return (file_exists(self::filename_format($key))) ? true : false;
   }
-  
+
   public static function reset() {
-    foreach(glob(self::CACHEDIR . "*.php") as $cachename)
-      unlink($cachename);
+    foreach (glob(self::CACHEDIR . "*") as $filename)
+      unlink($filename);
   }
 
   private static function filename_format($key) {
-    $scriptname = preg_replace('/[^0-9a-z\.\_\-]/i', '', strtolower($_SERVER["SCRIPT_FILENAME"]));
+    $scriptname = preg_replace('/[^0-9a-z\.\_\-]/i', '', strtolower($_SERVER["REQUEST_URI"]));
     return self::CACHEDIR . md5($scriptname . $key);
   }
-  
+
   private static function storage_key() {
     return "_cache";
   }
