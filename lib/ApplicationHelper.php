86c51ef2d53c602fca4077962b40deee4e672d8b
Gökhan Demir <mail@gdemir.me>
2017-11-30 14:14:43 +0300

    Update ApplicationHelper.php

--- lib/ApplicationHelper.php
+++ lib/ApplicationHelper.php
@@ -1,24 +1,25 @@
 <?php
 
 class ApplicationHelper {
+	
+  const HELPERPATH = "app/helpers/";
+	
+  public static function load($helpers) {
+    if (is_array($helpers)) {
+      foreach ($helpers as $helper) {
+        $helper_path = self::HELPERPATH . $helper . "Helper.php";
+        if (!file_exists($helper_path))
+          throw new Exception("Helper dosyası mevcut değil → " . $helper_path);
+        require_once $helper_path;
+      }
+    } elseif ($helpers == "all") {
+      foreach (glob($self::HELPERPATH . "*.php") as $class) {
+        require_once $class;
+      }
+    } else {
+      throw new Exception("Helper methodunda bilinmeyen parametre → " . $helper);
+    }
+  }
 
-	const HELPERPATH = "app/helpers/";
-
-	public static function load($helpers) {
-		if (is_array($helpers)) {
-			foreach ($helpers as $helper) {
-				$helper_path = self::HELPERPATH . $helper . "Helper.php";
-				if (!file_exists($helper_path))
-					throw new Exception("Helper dosyası mevcut değil → " . $helper_path);
-				require_once $helper_path;
-			}
-		} elseif ($helpers == "all") {
-			foreach (glob($self::HELPERPATH . "*.php") as $class) {
-				require_once $class;
-			}
-		} else {
-			throw new Exception("Helper methodunda bilinmeyen parametre → " . $helper);
-		}
-	}
 }
 ?>
