c5d39df4896bf6794dd0deb585ec371cc51cc219
Gökhan Demir <gdemir3327@gmail.com>
2019-02-05 14:15:44 +0300

    Update ApplicationHelper.php

--- lib/ApplicationHelper.php
+++ lib/ApplicationHelper.php
@@ -1,19 +1,22 @@
 <?php
 
 class ApplicationHelper {
-	
+
   const HELPERPATH = "app/helpers/";
-	
+
+  const HELPERALLFILE = "all";
+  const HELPERSUBNAME = "Helper";
+
   public static function load($helpers) {
     if (is_array($helpers)) {
       foreach ($helpers as $helper) {
-        $helper_path = self::HELPERPATH . $helper . "Helper.php";
+        $helper_path = self::HELPERPATH . $helper . self::HELPERSUBNAME . ".php";
         if (!file_exists($helper_path))
           throw new Exception("Helper dosyası mevcut değil → " . $helper_path);
         require_once $helper_path;
       }
-    } elseif ($helpers == "all") {
-      foreach (glob($self::HELPERPATH . "*.php") as $class) {
+    } elseif ($helpers == self::HELPERALLFILE) {
+      foreach (glob($self::HELPERPATH . "*" . self::HELPERSUBNAME . ".php") as $class) {
         require_once $class;
       }
     } else {
