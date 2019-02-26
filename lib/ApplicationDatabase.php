319c8289c694b5f8beece696af5147aa2d5f8689
Gökhan Demir <gdemir3327@gmail.com>
2019-02-26 15:30:56 +0300

    adapter for Databaser driver

--- lib/ApplicationDatabase.php
+++ lib/ApplicationDatabase.php
@@ -13,13 +13,13 @@ class ApplicationDatabase {
 
     try {
       if (!isset(self::$_connection)) {
-        self::$_connection = new PDO("mysql:host={$host};dbname={$name}", $user, $pass);
+        self::$_connection = new PDO("{$adapter}:host={$hostname};dbname={$database}", $username, $password);
 
         // configuration database
-        self::$_connection->query('set names "utf8"');
-        self::$_connection->query('set character set "utf8"');
-        self::$_connection->query('set collation_connection = "utf8_general_ci"');
-        self::$_connection->query('set collation-server = "utf8_general_ci"');
+        self::$_connection->query("set names 'utf8'");
+        self::$_connection->query("set character set 'utf8'");
+        self::$_connection->query("set collation_connection = 'utf8_general_ci'");
+        self::$_connection->query("set collation-server = 'utf8_general_ci'");
         self::$_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       }
     } catch (PDOException $e) {
