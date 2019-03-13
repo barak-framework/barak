bbb9381c4c290b7b56036fe738b242af7d59c4ce
Gökhan Demir <gdemir3327@gmail.com>
2019-03-13 23:08:34 +0300

    Update ApplicationRequest.php

--- lib/ApplicationRequest.php
+++ lib/ApplicationRequest.php
@@ -5,23 +5,29 @@ class ApplicationRequest {
   public $ip; // requester ip
   public $rule; // request rule
   public $method; // request method
+  public $datetime; // request datetime
 
   final public function __construct() { // genişletilemez fonksiyon
-  	$this->ip = self::_ip();
-  	$this->rule = self::_rule();
-  	$this->method = self::_method();
+    $this->ip = self::_ip();
+    $this->rule = self::_rule();
+    $this->method = self::_method();
+    $this->datetime = self::_datetime();
   }
 
-  private function _ip() {
-  	return $_SERVER['REMOTE_ADDR'];
+  private static function _ip() {
+    return $_SERVER['REMOTE_ADDR'];
   }
 
-  private function _rule() {
-  	return $_SERVER['REQUEST_URI'];
+  private static function _rule() {
+    return $_SERVER['REQUEST_URI'];
   }
 
-  private function _method() {
-  	return $_SERVER['REQUEST_METHOD'];
+  private static function _method() {
+    return $_SERVER['REQUEST_METHOD'];
+  }
+
+  private static function _datetime() {
+    return date("Y-m-d H:i:s");
   }
 
 }
